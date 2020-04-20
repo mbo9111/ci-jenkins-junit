af36a1259248a7426f3fe55a9a7e774c2ef08063
mbo9111 <61522007+mbo9111@users.noreply.github.com>
2020-04-20 03:16:03 +0100

    Update Calculator.java

--- src/main/java/com/example/math/Calculator.java
+++ src/main/java/com/example/math/Calculator.java
@@ -1,13 +1,6 @@
-package com.example.math;
 
-public class Calculator {
-
-    public int add(int a, int b) {
-        return a + b;
+@Test
+    public void multiply() {
+        Calculator calculator = new Calculator();
+        assertEquals(20, calculator.multiply(10, 2));
     }
-
-    public int subtract(int a, int b) {
-        return a - b;
-    }
-
-}
