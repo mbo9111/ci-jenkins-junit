df240b13c4cdbe270833fe3363b4eb79a49f1a11
mbo9111 <61522007+mbo9111@users.noreply.github.com>
2020-04-20 03:16:44 +0100

    Update Calculator.java

--- src/main/java/com/example/math/Calculator.java
+++ src/main/java/com/example/math/Calculator.java
@@ -4,3 +4,7 @@
         Calculator calculator = new Calculator();
         assertEquals(20, calculator.multiply(10, 2));
     }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
